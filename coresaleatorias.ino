4591553544312a3c79d025dc6e3fc260c969d77c
rian magic box <70152017+rianmagicbox@users.noreply.github.com>
2021-04-17 13:19:11 -0300

    Create coresaleatorias.ino

---
+++ coresaleatorias.ino
@@ -0,0 +1,89 @@
+#include <FastLED.h>
+
+#define LED_PIN  3
+
+#define COLOR_ORDER GRB
+
+
+#define CHIPSET     WS2811
+
+#define BRIGHTNESS 64
+
+CRGB branco = CRGB ( 255, 255, 255);
+
+CRGB vermelho = CRGB ( 255, 0, 0);                        
+
+const uint8_t kMatrixWidth = 8;
+const uint8_t kMatrixHeight = 8;
+
+
+const bool    kMatrixSerpentineLayout = false;
+const bool    kMatrixVertical = false;
+
+
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+
+  if( kMatrixSerpentineLayout == false) {
+    if (kMatrixVertical == false) {
+      i = (y * kMatrixWidth) + x;
+    } else {
+      i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
+    }
+  }
+
+  if( kMatrixSerpentineLayout == true) {
+    if (kMatrixVertical == false) {
+      if( y & 0x01) {
+        // Odd rows run backwards
+        uint8_t reverseX = (kMatrixWidth - 1) - x;
+        i = (y * kMatrixWidth) + reverseX;
+      } else {
+        // Even rows run forwards
+        i = (y * kMatrixWidth) + x;
+      }
+    } else { // vertical positioning
+      if ( x & 0x01) {
+        i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
+      } else {
+        i = kMatrixHeight * (kMatrixWidth - x) - (y+1);
+      }
+    }
+  }
+
+  return i;
+}
+
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+CRGB* const leds( leds_plus_safety_pixel + 1);
+
+uint16_t XYsafe( uint8_t x, uint8_t y)
+{
+  if( x >= kMatrixWidth) return -1;
+  if( y >= kMatrixHeight) return -1;
+  return XY(x,y);
+}
+
+
+void loop()
+{
+    
+    for( int led = 0; led < 64; led++){
+
+     leds[led] = CRGB ( rand() % 255, rand() % 255, rand() % 255 );
+
+    }
+    
+    FastLED.show();
+
+    delay(300);
+  
+}
+
+
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+  FastLED.setBrightness( BRIGHTNESS );
+}
